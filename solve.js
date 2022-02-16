ac11877400956e1a642801c52293d606b2f7b3b0
jayrunkel <jayrunkel@gmail.com>
2022-02-16 17:25:21 -0500

    converted from mongosh to nodejs and added the ability to provide information via command line arguments

--- solve.js
+++ solve.js
@@ -1,26 +1,45 @@
-const db = db.getSiblingDB("dictionary")
-const col = db.getCollection("words")
+
+import  yargs from "yargs" // "yargs/yargs"
+import { MongoClient } from "mongodb";
+import { hideBin } from 'yargs/helpers'
+const argv = yargs(hideBin(process.argv)).argv
+
 
 const config = {
-	wordLength: 5,
-	resultLimit: 20,
-	mustHaveLetters : ["c", "n", "i"],
-	mustNotHaveLetters : ["r", "a", "e", "m"],
-	badPositions : {
+	uri : argv.uri || "mongodb://locahost:27017",
+	db : argv.db || "dictionary",
+	collection : argv.collection || "ospd",
+	wordLength: argv.wordLenth || 5,
+	resultLimit: argv.resultLimit || 20,
+	mustHaveLetters : argv.mustHaveLetters ? JSON.parse(argv.mustHaveLetters) : [],
+	mustNotHaveLetters : argv.mustNotHaveLetters ? JSON.parse(argv.mustNotHaveLetters) : [],
+	badPositions : argv.badPositions ? JSON.parse(argv.badPositions) : {
 		0 : [],
-		1 : ["r", "u"],
-		2 : ["a", "m"],
-		3 : ["n"],
-		4 : ["e", "n"]
+		1 : [],
+		2 : [],
+		3 : [],
+		4 : []
 	},
-	goodPositions : {
-		0 : "c",
+	goodPositions : argv.goodPositions ? JSON.parse(argv.goodPositions) : {
+		0 : null,
 		1 : null,
 		2 : null,
-		3 : "i",
+		3 : null,
 		4 : null
 	}
 }
+
+console.log(config)
+
+async function connectToDatabase() {
+	const c = new MongoClient(config.uri)
+	await c.connect()
+	return c
+}
+/*
+const db = db.getSiblingDB(config.db)
+const col = db.getCollection(config.collection)
+*/
 
 const enrichWordsStage =
   {
@@ -61,7 +80,7 @@ function buildMustNotHaveTest(configObj) {
 
 function buildGoodPositionsTest(configObj) {
 	var goodPosTests = []
-	for (pos = 0; pos < config.wordLength; pos++) {
+	for (let pos = 0; pos < config.wordLength; pos++) {
 		if (configObj.goodPositions[pos]) {
 			goodPosTests.push({'$eq': [configObj.goodPositions[pos], {'$arrayElemAt': ['$letters', pos]}]})
 		}
@@ -73,7 +92,7 @@ function buildGoodPositionsTest(configObj) {
 
 function buildBadPositionsTest(configObj) {
 	var badPosTests = []
-	for (pos = 0; pos < config.wordLength; pos++) {
+	for (let pos = 0; pos < config.wordLength; pos++) {
 		configObj.badPositions[pos].map(letter => {
 			badPosTests.push({'$not': {'$eq': [letter, {'$arrayElemAt': ['$letters', pos]}]}})
 		})
@@ -96,12 +115,60 @@ const matchStage = {
     }
 }
 
-const limitStage = {$limit : config.resultLimit}
+const facetStage = {
+	$facet: {
+		count: [
+			{
+				$count: 'count'
+			}
+		],
+		guesses: [
+			{
+				$project: {
+					_id: 0,
+					word: 1
+				}
+			},
+			{
+				$limit: config.resultLimit
+			}
+		]
+	}
+}
 
-const pipeline = [enrichWordsStage, matchStage, limitStage]
+const cleanUpStage = {
+	$addFields: {
+		count : {
+			$let : {
+				vars : {countObj : {$arrayElemAt : ["$count", 0]}},
+					in : "$$countObj.count"
+			}
+		},
+		resultLimit : config.resultLimit,
+		guesses: {
+			$map: {
+				input: '$guesses',
+				'in': '$$this.word'
+			}
+		}
+	}
+}
 
-printjson(pipeline)
+const pipeline = [enrichWordsStage, matchStage, facetStage, cleanUpStage]
 
-var result = col.aggregate(pipeline).toArray()
+console.log(pipeline)
 
-printjson(result)
+async function run() {
+	let client = await connectToDatabase()
+	let db = client.db(config.db)
+	let col = db.collection(config.collection)
+
+	var result = await col.aggregate(pipeline).toArray()
+	console.log(result[0])
+	client.close()
+}
+
+run()
+
+
+ 
