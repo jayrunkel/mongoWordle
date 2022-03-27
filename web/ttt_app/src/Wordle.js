524787857b5bac35ab83d25e90955178ab9ae89a
Jay Runkel <jayrunkel@gmail.com>
2022-03-26 20:33:00 -0400

    got the deletion of letters working

--- web/ttt_app/src/Wordle.js
+++ web/ttt_app/src/Wordle.js
@@ -84,27 +84,57 @@ class Wordle extends React.Component {
 		};
 	}
 	
-	keyHandler(event) {
-		const curGuessNum = this.state.guessNumber;
-		const curLetNum = this.state.letterNumber;
+    keyHandler(event) {
+	const curGuessNum = this.state.guessNumber;
+	const curLetNum = this.state.letterNumber;
+	let previousGuesses;
+	let previousLetters;
+	console.log("Start: curGuessNum: ", curGuessNum, "curLetNum: ", curLetNum);
+	if (event.key.length === 1 && (/[a-zA-Z]/).test(event.key)) {
+	    console.log("event.key: ", event.key);
+	    const letter = event.key.toUpperCase();
 
-		const previousGuesses = curGuessNum > 0 ? this.state.guesses.slice(0, curGuessNum) : [];
-		const previousLetters = curLetNum > 0 ? this.state.guesses[curGuessNum].slice(0, curLetNum) : [];
-		const curGuessLetter = {
-			letter: event.key,
-			inWord: false,
-			correctPos: false,
-		}
+	    previousGuesses = curGuessNum > 0 ? this.state.guesses.slice(0, curGuessNum) : [];
+	    previousLetters = curLetNum > 0 ? this.state.guesses[curGuessNum].slice(0, curLetNum) : [];
+	    const curGuessLetter = {
+		letter: letter,
+		inWord: false,
+		correctPos: false,
+	    }
 		
-		const curGuessLetters = previousLetters.concat([curGuessLetter]);
+	    const curGuessLetters = previousLetters.concat([curGuessLetter]);
 				
+	    this.setState({
+		guesses : previousGuesses.concat([curGuessLetters]),
+		letterNumber : curLetNum === WORD_LENGTH - 1 ? 0 : curLetNum + 1,
+		guessNumber : curLetNum === WORD_LENGTH - 1 ? curGuessNum + 1 : curGuessNum, 
+		lastKey: letter,
+	    });
+	} else if (event.key === "Delete" || event.key === "Backspace") {
+	    if (curGuessNum > 0 || curLetNum > 0) {
+
+		if (curGuessNum === 0) {
+		    previousGuesses = [];
+		} else if (curGuessNum === 1) {
+		    previousGuesses = curLetNum === 0 ? []: this.state.guesses.slice(0, curGuessNum)
+		} else {
+		    previousGuesses = curLetNum === 0 ? this.state.guesses.slice(0, curGuessNum - 1) : this.state.guesses.slice(0, curGuessNum);
+		}
+
+		previousLetters = curLetNum > 0 ? this.state.guesses[curGuessNum].slice(0, curLetNum - 1) : this.state.guesses[curGuessNum - 1].slice(0, WORD_LENGTH - 1);
+
+		console.log("processing request");
 		this.setState({
-			guesses : previousGuesses.concat([curGuessLetters]),
-			letterNumber : curLetNum === WORD_LENGTH - 1 ? 0 : curLetNum + 1,
-			guessNumber : curLetNum === WORD_LENGTH - 1 ? curGuessNum + 1 : curGuessNum, 
-			lastKey: event.key,
+		    guesses : previousGuesses.concat([previousLetters]),
+		    letterNumber : curLetNum === 0 ? WORD_LENGTH - 1 : curLetNum - 1,
+		    guessNumber : curLetNum === 0 ? curGuessNum - 1 : curGuessNum, 
+		    lastKey: "Backspace"
 		});
+		console.log("END: curGuessNum: ", this.state.guessNumber, "curLetNum: ", this.state.letterNumber);
+	    }
+	    event.preventDefault();
 	}
+    }
 
   handleClick(clkGuessNum, clkLetterNum) {
 		console.log("clkGuessNum: ", clkGuessNum, " clkLetterNum: ", clkLetterNum);
@@ -144,17 +174,29 @@ class Wordle extends React.Component {
         'Go to move #' + move :
         'Go to game start';
 */
-    return (
-			<div className="wordleGame" tabIndex="-1" onKeyDown={(e) => this.keyHandler(e)}>
-				<div><label>{this.state.lastKey}</label></div>
-				<div className="game-board">
-					{
-						this.state.guesses.map((guess, guessNum) => {
-							return <Row key={guessNum} letters={guess} handleClick={(l) => this.handleClick(guessNum, l)} />;
-											 })
-					}
-				</div>
-			</div>
+      return (
+	  <div className="all"  tabIndex="-1" onKeyDown={(e) => this.keyHandler(e)}>
+	      <div className="wordleGame">
+		  <div className="game-board">
+		      {
+			  this.state.guesses.map((guess, guessNum) => {
+			      return <Row key={guessNum} letters={guess} handleClick={(l) => this.handleClick(guessNum, l)} />;
+			  })
+		      }
+		  </div>
+	      </div>
+	      <div className="guessSection">
+		  <label>Total Matches: 9999</label>
+		  <label>Matches Shown: 99</label>
+		  <div className="matchList">
+		      <ul>
+			  <li>chaos</li>
+			  <li>chest</li>
+			  <li>chart</li>
+		      </ul>
+		  </div>
+	      </div>
+	  </div>
     );
   }
 }
